d47fa9763c188eda45a57f6c8d11b0e9a78d5a4a
Dochi08 <123103046+Dochi08@users.noreply.github.com>
2023-03-23 08:48:02 -0700

    small changes with goomba and also changes with animation on mario

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "mario":return img`
+. . . . . 2 2 2 2 2 2 2 . . . . 
+. . . 2 2 2 2 2 2 2 2 2 . . . . 
+. . . 2 1 1 2 2 2 2 2 2 . . . . 
+. . 2 2 2 2 2 2 2 2 2 2 . . . . 
+. . . . d d d d d d d d . . . . 
+. . . . d 1 8 d d 8 1 d . . . . 
+. . . . d d d d d d d d . . . . 
+. . . . . d d d d d d . . . . . 
+. . . . 2 2 2 2 2 2 2 2 . . . . 
+. . . . 2 . 5 2 2 5 . 2 . . . . 
+. . . . 1 . 8 2 2 8 . 1 . . . . 
+. . . . . . 8 8 8 8 . . . . . . 
+. . . . . . 8 8 8 8 . . . . . . 
+. . . . . . 8 . . 8 . . . . . . 
+. . . . . . 8 . . 8 . . . . . . 
+. . . . . e e . . e e . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
--- main.ts
+++ main.ts
@@ -1,6 +1,4 @@
-/**
- * I want to make it so it looks like a goomba comes out of the pipe.
- */
+// I want to make it so it looks like a goomba comes out of the pipe.
 function Goomba () {
     goomba = sprites.create(img`
         . . . . . . . . . . . . . . . . 
@@ -22,6 +20,9 @@ function Goomba () {
         `, SpriteKind.Enemy)
     tiles.placeOnRandomTile(goomba, assets.tile`myTile`)
     goomba.y += -20
+    pause(100)
+    goomba.ay += 150
+    goomba.setVelocity(randint(-50, 50), 50)
 }
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile1`, function (sprite, location) {
     game.gameOver(true)
@@ -65,22 +66,22 @@ function Mario_Jump () {
         animation.runImageAnimation(
         mario,
         [img`
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
+            . . . . . 2 2 2 2 2 2 2 . . . . 
+            . . . 2 2 2 2 2 2 2 2 2 . . . . 
+            . . . 2 1 1 2 2 2 2 2 2 . . . . 
+            . . 2 2 2 2 2 2 2 2 2 2 . . . . 
+            . . . . d d d d d d d d . . . . 
+            . . . . d 1 8 d d 8 1 d . . . . 
+            . . . . d d d d d d d d . 1 . . 
+            . . . . . d d d d d d . . 2 . . 
+            . . . . 2 2 2 2 2 2 2 2 2 2 . . 
+            . . . . 2 . 5 2 2 5 . . . . . . 
+            . . . . 1 . 8 2 2 8 . . . . . . 
+            . . . . . . 8 8 8 8 . . . . . . 
+            . . . . . . 8 8 8 8 8 8 . . . . 
+            . . . . . . 8 . . . . e e . . . 
+            . . . . . . 8 8 . . . . . . . . 
+            . . . . . . . e e . . . . . . . 
             `],
         200,
         true
@@ -112,45 +113,25 @@ function Use_later () {
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile2`, function (sprite, location) {
     game.gameOver(true)
 })
-/**
- * I want to make it so if you jump on the goomba you kill him but if you touch him from the sides you lose.
- */
+// I want to make it so if you jump on the goomba you kill him but if you touch him from the sides you lose.
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
-    if (true) {
-    	
+    if (goomba.y < mario.y + mario.height) {
+        sprites.destroy(goomba)
+        info.changeScoreBy(1)
     } else {
-    	
+        sprites.destroy(mario)
     }
 })
 let luigi: Sprite = null
 let goomba: Sprite = null
 let mario: Sprite = null
-mario = sprites.create(img`
-    . . . . . 2 2 2 2 2 2 2 . . . . 
-    . . . 2 2 2 2 2 2 2 2 2 . . . . 
-    . . . 2 1 1 2 2 2 2 2 2 . . . . 
-    . . 2 2 2 2 2 2 2 2 2 2 . . . . 
-    . . . . d d d d d d d d . . . . 
-    . . . . d 1 8 d d 8 1 d . . . . 
-    . . . . d d d d d d d d . . . . 
-    . . . . . d d d d d d . . . . . 
-    . . . . 2 2 2 2 2 2 2 2 . . . . 
-    . . . . 2 . 5 2 2 5 . 2 . . . . 
-    . . . . 1 . 8 2 2 8 . 1 . . . . 
-    . . . . . . 8 8 8 8 . . . . . . 
-    . . . . . . 8 8 8 8 . . . . . . 
-    . . . . . . 8 . . 8 . . . . . . 
-    . . . . . . 8 . . 8 . . . . . . 
-    . . . . . e e . . e e . . . . . 
-    `, SpriteKind.Player)
+mario = sprites.create(assets.image`mario`, SpriteKind.Player)
 controller.moveSprite(mario, 100, 0)
 mario.ay = 250
 tiles.setCurrentTilemap(tilemap`level1`)
+info.setScore(0)
 tiles.placeOnRandomTile(mario, assets.tile`myTile4`)
 scene.cameraFollowSprite(mario)
-game.onUpdateInterval(2000, function () {
+game.onUpdateInterval(5000, function () {
     Goomba()
-})
-game.onUpdateInterval(100, function () {
-	
 })
